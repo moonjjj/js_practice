525aa90d9778526c98003d752912755f70dad8ed
moonjjj <mjesun1@naver.com>
2020-11-07 12:58:45 +0900

    commit

---
+++ clock.js
@@ -0,0 +1,20 @@
+const clockContainer = document.querySelector(".js-clock");
+const clockTitle = clockContainer.querySelector("h1");
+
+
+function getTime() {
+    const date= new Date();
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+    const seconds = date.getSeconds();
+    clockTitle.innerText = `${hours<10 ? `0${hours}` : hours}:${
+        minutes<10 ? `0${minutes}` : minutes}:${
+            seconds<10 ? `0${seconds}` : seconds}`; 
+    //삼항연산자를 사용하였다. 만약 seconds가 10보다 작다면 앞에0을 붙히고 그게 아니라면(else) 그냥 보여주기
+}
+//setInterval()이란 함수는 첫번째 인자로 실행할 함수를 받고, 두번째 인자로 실행하고싶은 시간(ms)로
+function init() {
+    getTime();
+    setInterval(getTime, 1000);
+}
+init();
---
+++ greeting.js
@@ -0,0 +1,47 @@
+const form = document.querySelector(".js-form");
+const input = form.querySelector("input");
+const greeting = document.querySelector(".js-greetings");
+const USER_LS="currentUser";
+const SHOWING_CN="showing"; //showing이라는 클래스를 classList.add로 가져올 것이다 (.showing에 대한 css가 이미 적혀있음)
+
+function saveName(text){
+    localStorage.setItem(USER_LS, text);
+}
+
+function handlerSubmit(e){
+    e.preventDefault(); //event의 기본적인 속성을 바꾼다.
+    const currentValue = input.value;
+    console.log(currentValue);
+    paingGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handlerSubmit);
+}
+
+function paingGreeting(text){
+    form.classList.remove(SHOWING_CN);
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerText = `hello ${text}`;
+}
+
+function loadName() {
+    const currentUser = localStorage.getItem(USER_LS); //localstorage(서버)에 userls를 가져와서 currentuser에 담는다
+
+    if(currentUser === null){
+        askForName();
+    } else{
+        paingGreeting(currentUser); //currentuser의 value값을 매개변수로 가져감.
+    }
+}
+
+function init(){
+ loadName();
+}
+
+init();
+//init() -> loadname() -> 로컬스토리지에서 user_ls(사용자 이름)을 가져와서 없다면 go askforname 있다면 paingGreeting(사용자이름을 매개변수로 가져감)이동
+//없을시-> askforname() 숨겨놨던 form태그를 보이게 만들고, submit enent를 추가한다.->서브밋(엔터)를 할 경우에 
+//있을시-> paingGreeting(사용자이름)
