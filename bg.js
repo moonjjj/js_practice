d6bfee8b821ff53c5eec2dd782fbf6f6a481ca5d
moonjjj <mjesun1@naver.com>
2020-11-10 14:47:11 +0900

    commit

--- bg.js
+++ bg.js
@@ -8,7 +8,7 @@ function handleImgLoad(){
 
 function paintImage(imgNumber){
     const image = new Image();
-    image.src = `images/${imgNumber}+1.jpg`;
+    image.src = `images/${imgNumber+1}.jpg`;
     image.classList.add('bgImage');
     body.appendChild(image);
     image.addEventListener("loaded", handleImgLoad);
