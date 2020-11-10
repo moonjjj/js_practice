b9cedcb38465d94f279559ce0cd68419841ac56e
moonjjj <mjesun1@naver.com>
2020-11-10 14:40:38 +0900

    commit

--- bg.js
+++ bg.js
@@ -8,14 +8,14 @@ function handleImgLoad(){
 
 function paintImage(imgNumber){
     const image = new Image();
-    image.src = `images/${imgNumber}.jpg`;
+    image.src = `images/${imgNumber}+1.jpg`;
     image.classList.add('bgImage');
     body.appendChild(image);
     image.addEventListener("loaded", handleImgLoad);
 }
 
 function genRandom(){
-    const number = Math.floor(Math.random()*IMG_NUMBER+1);
+    const number = Math.floor(Math.random()*IMG_NUMBER);
     return number;
 }
 
